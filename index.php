dadc1a1fa0e6128f9285c6739d48f9df992cb886
Liliadetassia <lylyatavares@2gmail.com>
2022-02-24 17:45:38 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet"> 
+    <title>Lília de Tássia - Tema</title>
+</head>
+<body>
+<section class="topo">
+    <div class="center">
+        <header>
+          <div class="logo"><img src="images/logo.png" /></div><!-- logo --->
+        </header>   
+        <ul class="menu-dekstop">
+           <li><a href="#">Home</a></li>
+           <li><a href="#">Sobre</a></li>
+           <li><a href="#">Contato</a></li>
+        </ul><!-- menu-dekstop --->
+        <div class="clear"></div><!-- clear --->
+    </div><!-- center --->
+</section><!-- topo --->
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
